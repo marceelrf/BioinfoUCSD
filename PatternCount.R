9e65b12469dc580e419b671aceb310503b701d38
Marcel Ferreira <48413106+marceelrf@users.noreply.github.com>
2020-07-12 20:58:22 -0300

    Add files via upload

---
+++ PatternCount.R
@@ -0,0 +1,10 @@
+PatternCount <- function(Text, Pattern){
+  require(stringr,quietly = TRUE)
+  contar <- 0
+  for(i in 1:(str_length(Text)-str_length(Pattern))){
+    if(str_sub(Text,start = i,end = i+str_length(Pattern)-1) == Pattern){
+      contar <- contar +1
+    }
+  }
+  return(contar)
+}
