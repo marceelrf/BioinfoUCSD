6de861ea5936fe42c7e5531cd4367f9331dac8dc
Marcel Ferreira <48413106+marceelrf@users.noreply.github.com>
2020-07-13 16:51:46 -0300

    Add files via upload

---
+++ ReverseComplement.R
@@ -0,0 +1,16 @@
+ReverseComplement <- function(Text){
+  strand <- NULL
+  
+  for(i in 1:(str_length(Text))){
+    strand[i] <- ifelse(str_sub(Text,start = i,end = i) == "A",
+                        yes = "T",
+                        no = ifelse(str_sub(Text,start = i,end = i) == "T",
+                                    yes = "A",
+                                    no = ifelse(str_sub(Text,start = i,end = i) == "G",
+                                                yes = "C",
+                                                no = "G")))
+  }
+  strand <- rev(strand)
+  strand <- str_c(strand,collapse = "")
+  return(strand)
+}
